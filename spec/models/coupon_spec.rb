fbdd4cb235c571d617acb3d9f79ebd75f8303d70
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-09-20 15:22:23 -0400

    Fixed

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
+require 'rails_helper'
+
 RSpec.describe Coupon, type: :model do
-  before(:each) do 
-    @merchant = Merchant.create!(name: "Sample Merchant")
-  end 
-  describe 'relationships' do 
-    it {should belong_to(:merchant)}
-    it {should have_many(:invoices)}
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:code) }
-    it { should validate_uniqueness_of(:code).scoped_to(:merchant_id) } # Scope to merchant_id if needed
-    it { should validate_inclusion_of(:active).in_array([true, false]) }
+  before do
+    @merchant = Merchant.create(name: "Test Merchant")
+  end
+
+  it { should belong_to(:merchant) }
+  it { should have_many(:invoices) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:code) }
+  it { should validate_inclusion_of(:active).in_array([true, false]) }
+
+  it 'validates uniqueness of code scoped to merchant_id' do
+    Coupon.create!(name: "Existing Coupon", code: "UNIQUECODE", active: true, merchant: @merchant)
+    new_coupon = Coupon.new(name: "New Coupon", code: "UNIQUECODE", active: true, merchant: @merchant)
+    
+    expect(new_coupon).not_to be_valid
+    expect(new_coupon.errors[:code]).to include("has already been taken")
   end
 end
