f6fc3bcee168748d2e09cb5ab701144671e37d52
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-09-20 14:51:07 -0400

    Fixed

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Coupon, type: :model do
   before(:each) do 
     @merchant = Merchant.create!(name: "Sample Merchant")
-  end
+  end 
   describe 'relationships' do 
     it {should belong_to(:merchant)}
     it {should have_many(:invoices)}
@@ -11,27 +11,34 @@ RSpec.describe Coupon, type: :model do
 
   describe 'validations' do
     it 'is valid with valid attributes' do
-      coupon = Coupon.new(name: "Sample Coupon", code: "COUPON001",discount_value: 1, active: true, merchant: @merchant)
+      coupon = Coupon.create!(name: "Sample Coupon", code: "COUPON001", discount_value: 1, active: true, merchant: @merchant)
       expect(coupon).to be_valid
     end
 
     it 'is not valid without a name' do
-      coupon = Coupon.new(name: nil, code: "COUPON001", merchant: @merchant)
-      expect(coupon).to_not be_valid
-      expect(coupon.errors[:name]).to include("can't be blank")
+      expect {
+        Coupon.create!(name: nil, code: "COUPON001", merchant: @merchant)
+      }.to raise_error(ActiveRecord::RecordInvalid) do |error|
+        expect(error.record.errors[:name]).to include("can't be blank")
+      end
     end
 
     it 'is not valid without a code' do
-      coupon = Coupon.new(name: "Sample Coupon", code: nil, merchant: @merchant)
-      expect(coupon).to_not be_valid
-      expect(coupon.errors[:code]).to include("can't be blank")
+      expect {
+        Coupon.create!(name: "Sample Coupon", code: nil, merchant: @merchant)
+      }.to raise_error(ActiveRecord::RecordInvalid) do |error|
+        expect(error.record.errors[:code]).to include("can't be blank")
+      end
     end
 
     it 'is not valid with a duplicate code for the same merchant' do
-      Coupon.create!(name: "Sample Coupon", code: "COUPON001",discount_value: 1, active: true, merchant: @merchant)
-      coupon = Coupon.new(name: "New Coupon", code: "COUPON001", active: true, merchant: @merchant)
-      expect(coupon).to_not be_valid
-      expect(coupon.errors[:code]).to include("has already been taken")
+      Coupon.create!(name: "Sample Coupon", code: "COUPON001", discount_value: 1, active: true, merchant: @merchant)
+      
+      expect {
+        Coupon.create!(name: "New Coupon", code: "COUPON001", active: true, merchant: @merchant)
+      }.to raise_error(ActiveRecord::RecordInvalid) do |error|
+        expect(error.record.errors[:code]).to include("has already been taken")
+      end
     end
   end
 end
