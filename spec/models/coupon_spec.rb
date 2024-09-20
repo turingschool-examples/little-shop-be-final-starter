077b1b163b499f347c3f9b5981c6b961906037ac
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-09-20 15:12:13 -0400

    refactored coupon model spec

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -7,38 +7,9 @@ RSpec.describe Coupon, type: :model do
   describe 'relationships' do 
     it {should belong_to(:merchant)}
     it {should have_many(:invoices)}
-  end
-
-  describe 'validations' do
-    it 'is valid with valid attributes' do
-      coupon = Coupon.create!(name: "Sample Coupon", code: "COUPON001", discount_value: 1, active: true, merchant: @merchant)
-      expect(coupon).to be_valid
-    end
-
-    it 'is not valid without a name' do
-      expect {
-        Coupon.create!(name: nil, code: "COUPON001", merchant: @merchant)
-      }.to raise_error(ActiveRecord::RecordInvalid) do |error|
-        expect(error.record.errors[:name]).to include("can't be blank")
-      end
-    end
-
-    it 'is not valid without a code' do
-      expect {
-        Coupon.create!(name: "Sample Coupon", code: nil, merchant: @merchant)
-      }.to raise_error(ActiveRecord::RecordInvalid) do |error|
-        expect(error.record.errors[:code]).to include("can't be blank")
-      end
-    end
-
-    it 'is not valid with a duplicate code for the same merchant' do
-      Coupon.create!(name: "Sample Coupon", code: "COUPON001", discount_value: 1, active: true, merchant: @merchant)
-      
-      expect {
-        Coupon.create!(name: "New Coupon", code: "COUPON001", active: true, merchant: @merchant)
-      }.to raise_error(ActiveRecord::RecordInvalid) do |error|
-        expect(error.record.errors[:code]).to include("has already been taken")
-      end
-    end
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:code) }
+    it { should validate_uniqueness_of(:code).scoped_to(:merchant_id) } # Scope to merchant_id if needed
+    it { should validate_inclusion_of(:active).in_array([true, false]) }
   end
 end
