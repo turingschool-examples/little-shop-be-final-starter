71d123668a1c677ba5a11925f5a62aa8dbc0eb55
Jim Macur <jim.macur@gmail.com>
2024-09-20 13:04:12 -0600

    fix: fixes typos in routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :items do
         resources :find, only: :index, controller: :search, action: :show
-        resources :find_al only: :index, controller: :search
+        resources :find_all, only: :index, controller: :search
       end
       resources :items, except: [:new, :edit] do
         get "/merchant", to: "items/merchants#show"
