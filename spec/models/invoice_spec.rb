33fed6de363de5f7f8a774522041361d691ce7f8
Terra Manning <terradmanning@gmail.com>
2024-11-08 09:57:41 -0700

    feat: create invoice model test for associations with factory bot

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,7 +1,33 @@
 require "rails_helper"
 
-RSpec.describe Invoice do
-  it { should belong_to :merchant }
-  it { should belong_to :customer }
-  it { should validate_inclusion_of(:status).in_array(%w(shipped packaged returned)) }
+RSpec.describe Invoice, type: :model do
+  describe 'associations' do
+    it { should belong_to :merchant }
+    it { should belong_to :customer }
+    it { should belong_to(:coupon).optional }
+    it { should have_many(:invoice_items).dependent(:destroy)}
+    it { should have_many(:transactions).dependent(:destroy)}
+  end 
+
+  describe 'validations' do
+    it { should validate_inclusion_of(:status).in_array(%w(shipped packaged returned)) }
+  end
+
+  describe 'using FactoryBot' do
+    before(:each) do
+      @merchant = FactoryBot.create(:merchant)
+      @customer = FactoryBot.create(:customer)
+      @coupon= FactoryBot.create(:coupon, merchant: @merchant)
+      @invoice_with_coupon = FactoryBot.create(:invoice, merchant: @merchant, customer: @customer, coupon: @coupon)
+      @invoice_without_coupon = FactoryBot.create(:invoice, merchant: @merchant, customer: @customer, coupon: nil)
+    end
+
+    it 'can be created without a coupon' do
+      expect(@invoice_without_coupon.coupon).to be(nil)
+    end
+
+    it 'can be created with a coupon' do
+      expect(@invoice_with_coupon.coupon).to be(@coupon)
+    end
+  end
 end
