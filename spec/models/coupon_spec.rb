79f81c645afb122b80b84e0bc05b43ccd0b2c5ad
sethverrill <verrill.seth@gmail.com>
2024-11-09 12:09:50 -0700

    test: Add associations test and verify they are working

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Coupon, type: :model do
-  # let(:merchant) { create(:merchant) }
-  # let(:coupon) { create(:coupon, merchant: merchant) }
+  let(:merchant) { create(:merchant) }
+  let(:coupon) { create(:coupon, merchant: merchant) }
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
@@ -13,4 +13,9 @@ RSpec.describe Coupon, type: :model do
     it { should validate_presence_of(:discount_value) }
     it { should validate_numericality_of(:discount_value).is_greater_than(0) }
   end
+
+  describe 'associations' do 
+    it { should belong_to(:merchant) }
+    it { should have_many(:invoices) }
+  end  
 end
