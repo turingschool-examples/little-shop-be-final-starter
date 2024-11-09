1bd29765838d5478a826c92e11c6487966c0e173
sethverrill <verrill.seth@gmail.com>
2024-11-09 14:35:51 -0700

    feat: add traits to invoice factory bot to handle coupons

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     status { "shipped" }
     customer
     merchant
+
+    trait :with_coupon do
+      association :coupon, factory: :coupon
+    end
+
+    trait :without_coupon do
+      coupon_id { nil }
+    end
   end
 end
