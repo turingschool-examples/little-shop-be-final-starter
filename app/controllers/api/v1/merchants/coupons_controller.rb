bc3dc1b8fe2b19a0dad48a8400d5c63a048a5e51
Terra Manning <terradmanning@gmail.com>
2024-11-09 11:34:11 -0700

    refactor: modify code to reduce redundancy

--- app/controllers/api/v1/merchants/coupons_controller.rb
+++ app/controllers/api/v1/merchants/coupons_controller.rb
@@ -2,18 +2,8 @@ class Api::V1::Merchants::CouponsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def show
-    merchant = Merchant.find_by(id: params[:merchant_id])
-    if merchant.nil?
-      render json: ErrorSerializer.format_errors(['Your query could not be completed']), status: :not_found
-      return
-    end
-
-    coupon = merchant.coupons.find_by(id: params[:id])
-    if coupon.nil?
-      render json: ErrorSerializer.format_errors(['Your query could not be completed']), status: :not_found
-      return
-    end
-
+    merchant = Merchant.find(params[:merchant_id])
+    coupon = merchant.coupons.find(params[:id])
     render json: CouponSerializer.new(coupon), status: :ok
   end
 
