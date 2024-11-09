eaf5da1a299d7a18e269671ec550e7b62eaaecce
sethverrill <verrill.seth@gmail.com>
2024-11-09 11:03:53 -0700

    test: Add testing for model validations

---
+++ spec/models/coupon_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Coupon, type: :model do
+  # let(:merchant) { create(:merchant) }
+  # let(:coupon) { create(:coupon, merchant: merchant) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:code) }
+    it { should validate_uniqueness_of(:code) }
+    it { should validate_presence_of(:discount_type) }
+    it { should validate_inclusion_of(:discount_type).in_array(%w[percent_off dollar_off]) }
+    it { should validate_presence_of(:discount_value) }
+    it { should validate_numericality_of(:discount_value).is_greater_than(0) }
+  end
+end
