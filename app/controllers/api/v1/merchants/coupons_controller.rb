99f9d76a87178bd7e2cd8361f5fa314d9d197b97
Terra Manning <terradmanning@gmail.com>
2024-11-10 04:28:37 -0700

    feat: this code is all currently working up through the create endpoint

--- app/controllers/api/v1/merchants/coupons_controller.rb
+++ app/controllers/api/v1/merchants/coupons_controller.rb
@@ -7,14 +7,28 @@ class Api::V1::Merchants::CouponsController < ApplicationController
     render json: CouponSerializer.new(coupons), status: :ok
   end
 
-
   def show
     merchant = Merchant.find(params[:merchant_id])
     coupon = merchant.coupons.find(params[:id])
     render json: CouponSerializer.new(coupon), status: :ok
   end
 
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    coupon = merchant.coupons.new(coupon_params)
+
+    if coupon.save
+      render json: CouponSerializer.new(coupon), status: :created
+    else
+      render json: ErrorSerializer.format_validation_errors(coupon), status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def coupon_params
+    params.require(:coupon).permit(:name, :code, :discount_value, :discount_type, :active)
+  end
 
   def record_not_found
     render json: ErrorSerializer.format_errors(['Your query could not be completed']), status: :not_found
--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -2,16 +2,21 @@ class ErrorSerializer
   def self.format_errors(messages)
     {
       message: 'Your query could not be completed',
-      errors: messages
+      errors: Array(messages)
+    }
+  end
+
+  def self.format_validation_errors(record)
+    { 
+      message: "your query could not be completed", 
+      errors: record.errors.full_messages
     }
   end
 
   def self.format_invalid_search_response
-    { 
-      message: "your query could not be completed", 
-      errors: [
-        "invalid search params",
-      ] 
+    {
+      message: "your query could not be completed",
+      errors: ["invalid search params"]
     }
   end
 end
