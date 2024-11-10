b179326ea63529b275cfa0ce79e7255484240ae2
James-Cochran <jameslikesfruit@gmail.com>
2024-11-10 15:50:22 -0500

    Model tests for coupon validations

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,4 +3,6 @@ class Coupon < ApplicationRecord
 
   validates :name, presence: true
   validates :code, presence: true, uniqueness: true
+  validates :value, presence: true, numericality: { greater_than: 0 }
+  validates :active, inclusion: { in: [true, false] }
 end
