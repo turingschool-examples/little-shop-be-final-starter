609a0b7327fbb228531ebab729f869e050f3c4ec
sethverrill <verrill.seth@gmail.com>
2024-11-10 13:50:50 -0700

    test: add test for show method in coupon controller

--- spec/requests/api/v1/merchant/coupon_request_spec.rb
+++ spec/requests/api/v1/merchant/coupon_request_spec.rb
@@ -9,11 +9,28 @@ RSpec.describe 'Merchant Coupons endpoints' do
   end
 
   describe 'should return coupon info for given merchant' do
-     it 'returns all coupons for given merchant' do
+    it 'returns all coupons for given merchant' do
       get api_v1_merchant_coupons_path(merchant_id: merchant.id)
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body, symbolize_names: true)
       expect(json[:data].size).to eq(5)
-     end
+    end
+  end
+
+  describe 'should return a single coupon' do
+    let(:coupon) { create(:coupon, merchant: merchant) }
+
+    it 'returns details of a single coupon' do
+      get api_v1_merchant_coupon_path(merchant_id: merchant.id, id: coupon.id)
+      expect(response).to have_http_status(:ok)    
+      json = JSON.parse(response.body, symbolize_names: true)[:data]
+      
+      expect(json[:id]).to eq(coupon.id.to_s)
+      expect(json[:attributes][:name]).to eq(coupon.name)
+      expect(json[:attributes][:code]).to eq(coupon.code)
+      expect(json[:attributes][:discount_type]).to eq(coupon.discount_type)
+      expect(json[:attributes][:discount_value].to_f).to eq(coupon.discount_value.to_f)
+      expect(json[:attributes][:status]).to eq(coupon.status)
+    end
   end
 end
