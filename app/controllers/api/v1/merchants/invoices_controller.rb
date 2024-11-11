031fea19303f4f120df09858af264cb5c55ef15e
Terra Manning <terradmanning@gmail.com>
2024-11-11 11:11:08 -0700

    feat: create merchant invoice endpoint

--- app/controllers/api/v1/merchants/invoices_controller.rb
+++ app/controllers/api/v1/merchants/invoices_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::Merchants::InvoicesController < ApplicationController
   def index
     merchant = Merchant.find(params[:merchant_id])
-    if params[:status].present?
-      invoices = merchant.invoices_filtered_by_status(params[:status])
-    else
-      invoices = merchant.invoices
-    end
-    render json: InvoiceSerializer.new(invoices)
+
+    invoices = if params[:status].present?
+                 merchant.invoices_filtered_by_status(params[:status])
+               else
+                 merchant.invoices
+               end
+
+    render json: InvoiceSerializer.new(invoices), status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: ErrorSerializer.format_errors(['Merchant not found']), status: :not_found
   end
 end
