40c5e5261b56aa1d7ad5883f7d62993d9bbe18e9
sethverrill <verrill.seth@gmail.com>
2024-11-11 12:33:43 -0700

    test: add tests to verify coupon handling on invoices

--- spec/requests/api/v1/merchant/invoices_request_spec.rb
+++ spec/requests/api/v1/merchant/invoices_request_spec.rb
@@ -8,11 +8,15 @@ RSpec.describe "Merchant invoices endpoints" do
     @customer1 = Customer.create!(first_name: "Papa", last_name: "Gino")
     @customer2 = Customer.create!(first_name: "Jimmy", last_name: "John")
 
+    @coupon = Coupon.create!(name: "Something Off", code: "DISCOUNT10", discount_type: "percent_off", discount_value: 10.0, status: true, merchant: @merchant1)
+
     @invoice1 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "packaged")
     Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
     Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
     Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
     @invoice2 = Invoice.create!(customer: @customer1, merchant: @merchant2, status: "shipped")
+
+    @invoice_with_coupon = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped", coupon: @coupon)
   end
 
   it "should return all invoices for a given merchant based on status param" do
@@ -35,7 +39,7 @@ RSpec.describe "Merchant invoices endpoints" do
     json = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-    expect(json[:data].count).to eq(3)
+    expect(json[:data].count).to eq(4)
   end
 
   it "should only get invoices for merchant given" do
@@ -57,5 +61,26 @@ RSpec.describe "Merchant invoices endpoints" do
     expect(json[:message]).to eq("Your query could not be completed")
     expect(json[:errors]).to be_a Array
     expect(json[:errors].first).to eq("Couldn't find Merchant with 'id'=100000")
+  end
+
+  describe 'handling coupons' do
+    it "should add a coupon to an invoice" do
+      get "/api/v1/merchants/#{@merchant1.id}/invoices?status=shipped"
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      
+      invoice_with_coupon = json[:data].find { |invoice| invoice[:id] == @invoice_with_coupon.id.to_s }
+      expect(invoice_with_coupon[:attributes][:coupon_id]).to eq(@coupon.id)
+    end
+
+    it "should return null if the package is pending" do
+      get "/api/v1/merchants/#{@merchant1.id}/invoices?status=packaged"
+
+      json = JSON.parse(response.body, symbolize_names: true)
+    
+      expect(response).to be_successful
+      expect(json[:data].count).to eq(1)
+      expect(json[:data][0][:attributes][:coupon_id]).to be_nil
+    end
   end
 end
