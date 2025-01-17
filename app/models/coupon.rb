549bae6a266a5b0b59f474ee21d0b45b33c3fd2e
Matt Haefling <m.haefling@protonmail.com>
2025-01-16 18:53:44 -0700

    Added a bunch of coupons and new invoices to the seeds file

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,8 +1,8 @@
 class Coupon < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :code, presence: true, uniqueness: true
-  validates :dollar_off, presence: true, numericality: { only_float: true, allow_nil: true }
-  validates :percent_off, presence: true, numericality: { only_integer: true, allow_nil: true}
+  validates :dollar_off, presence: true, numericality: { only_float: true }, allow_blank: true
+  validates :percent_off, presence: true, numericality: { only_integer: true }, allow_blank: true
   validates :status, presence: true, inclusion: { in: ["active", "inactive"] }
   validates :merchant_id, presence: true, numericality: { only_integer: true }
   belongs_to :merchant
