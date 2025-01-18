0df48d1cd54d6ee4aa2d8e25b48cafcb2f8ac2ea
Kristin Weiland <kristinweiland@gmail.com>
2025-01-18 15:40:46 -0800

    Add an associations test for coupons model, update validations to full_name and :active, inclusion: { in: [true, false]} to correct logic errors and get association tests to pass.

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,9 +2,9 @@ class Coupon < ApplicationRecord
   belongs_to :merchant
   has_many :invoices
   
-  validates :name, presence: true
+  validates :full_name, presence: true
   validates :code, presence: true, uniqueness: true
-  validates :active, presence: true
+  validates :active, inclusion: { in: [true, false]}
   validates :merchant_id, presence: true
   validate :discount_type_constraints
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,9 +1,38 @@
 require "rails_helper"
 
-# RSpec.describe Coupon do
-#   describe 'associations' do
-#     it { should belong_to :merchant }
-    
+RSpec.describe Coupon do
+  describe 'associations' do
+    it "can be associated to a merchant" do 
+      test_merchant = Merchant.create!(name: "Test Merchant")
+      test_coupon = Coupon.create!(
+        full_name: "My first test coupon.",
+        code: "Ten percent",
+        percent_off: 10,
+        active: false,
+        merchant_id: test_merchant.id
+      )
+      expect(test_coupon.merchant).to eq(test_merchant)
+      # expect(test_merchant.coupons).to include(test_coupon) 
+    end
+  end
+    # it { should belong_to :merchant }
+    # it { should validate_inclusion_of(:status).in_array(%w(shipped packaged returned)) }
+  
+end
+
+# it "is valid with all attributes" do
+#   poster = Poster.create(
+#       name: "REGRET",
+#       description: "Hard work rarely pays off.",
+#       price: 89.00,
+#       year: 2018,
+#       vintage: true,
+#       img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+#       )
+
+#   expect(poster).to be_valid
+# end
+
 #   end
 
 #   describe 'validations' do
