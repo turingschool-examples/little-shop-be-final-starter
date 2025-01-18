7bf91dd0030335d3093ebc67612af00fa9bae2cc
Kristin Weiland <kristinweiland@gmail.com>
2025-01-18 15:47:40 -0800

    Refactor association test to utilize ShouldaMatchers.:

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -2,20 +2,21 @@ require "rails_helper"
 
 RSpec.describe Coupon do
   describe 'associations' do
-    it "can be associated to a merchant" do 
-      test_merchant = Merchant.create!(name: "Test Merchant")
-      test_coupon = Coupon.create!(
-        full_name: "My first test coupon.",
-        code: "Ten percent",
-        percent_off: 10,
-        active: false,
-        merchant_id: test_merchant.id
-      )
-      expect(test_coupon.merchant).to eq(test_merchant)
-      expect(test_merchant.coupons).to include(test_coupon) 
-    end
+    it { should belong_to :merchant }
+    # it "can be associated to a merchant" do 
+    #   test_merchant = Merchant.create!(name: "Test Merchant")
+    #   test_coupon = Coupon.create!(
+    #     full_name: "My first test coupon.",
+    #     code: "Ten percent",
+    #     percent_off: 10,
+    #     active: false,
+    #     merchant_id: test_merchant.id
+    #   )
+    #   expect(test_coupon.merchant).to eq(test_merchant)
+    #   expect(test_merchant.coupons).to include(test_coupon) 
+    # end
   end
-    # it { should belong_to :merchant }
+    
     # it { should validate_inclusion_of(:status).in_array(%w(shipped packaged returned)) }
   
 end
