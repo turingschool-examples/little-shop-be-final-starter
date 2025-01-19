65340caf50fd5c8fedfa1bf16480aa6124d46deb
Kristin Weiland <kristinweiland@gmail.com>
2025-01-18 22:27:22 -0800

    Refactor discount type validation tests to utilize merchant and coupon factories to clean up contaminated code.

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -47,58 +47,30 @@ RSpec.describe Coupon do
       expect(coupon.errors[:active]).to include("is not included in the list")
     end
 
-    xit "is valid when percent_off is not nill and dollar_off is nil" do
-      test_merchant = Merchant.create!(name: "Test Merchant")
-      test_coupon = Coupon.create!(
-        full_name: "My first test coupon.",
-        code: "Ten percent",
-        percent_off: 10,
-        dollar_off: nil,
-        active: false,
-        merchant_id: test_merchant.id
-      )
-      expect(test_coupon).to be_valid
+    it "is valid when percent_off is not nill and dollar_off is nil" do
+      coupon = build(:coupon, merchant: merchant, active: true, percent_off: 10, dollar_off: nil)
+       
+      expect(coupon).to be_valid
     end
 
-    xit "is valid when dollar_off is not nill and percent_off is nil" do
-      test_merchant = Merchant.create!(name: "Test Merchant")
-      test_coupon = Coupon.create!(
-        full_name: "My first test coupon.",
-        code: "Ten dollars off",
-        percent_off: nil,
-        dollar_off: 10,
-        active: false,
-        merchant_id: test_merchant.id
-      )
-      expect(test_coupon).to be_valid
+    it "is valid when dollar_off is not nill and percent_off is nil" do
+      coupon = build(:coupon, merchant: merchant, active: true, percent_off: nil, dollar_off: 10)
+       
+      expect(coupon).to be_valid
     end
 
-    xit "is invalid when both dollar_off and percent_off are nil" do
-      test_merchant = Merchant.create!(name: "Test Merchant")
-      test_coupon = Coupon.new(
-        full_name: "My first test coupon.",
-        code: "Ten dollars off",
-        percent_off: nil,
-        dollar_off: nil,
-        active: false,
-        merchant_id: test_merchant.id
-      )
-      expect(test_coupon).to_not be_valid
-      expect(test_coupon.errors[:base]).to include("one discount type (percent or dollar off) must be specified.")
+    it "is invalid when both dollar_off and percent_off are nil" do
+      coupon = build(:coupon, merchant: merchant, active: true, percent_off: nil, dollar_off: nil)
+  
+      expect(coupon).to_not be_valid
+      expect(coupon.errors[:base]).to include("one discount type (percent or dollar off) must be specified.")
     end
 
-    xit "is invalid when both dollar_off and percent_off are nil" do
-      test_merchant = Merchant.create!(name: "Test Merchant")
-      test_coupon = Coupon.new(
-        full_name: "My first test coupon.",
-        code: "Ten dollars off",
-        percent_off: 10,
-        dollar_off: 10,
-        active: false,
-        merchant_id: test_merchant.id
-      )
-      expect(test_coupon).to_not be_valid
-      expect(test_coupon.errors[:base]).to include("only one discount type (percent or dollar off) can be specified at a time.")
+    it "is invalid when both dollar_off and percent_off have a value" do
+      coupon = build(:coupon, merchant: merchant, active: true, percent_off: 10, dollar_off: 12)
+  
+      expect(coupon).to_not be_valid 
+      expect(coupon.errors[:base]).to include("only one discount type (percent or dollar off) can be specified at a time.")
     end
   end  
 end
