c757622cd5017854517daa04951be0fb666003e3
Kristin Weiland <kristinweiland@gmail.com>
2025-01-18 16:44:54 -0800

    Refactor validation tests for validate presence of name and code.

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Coupon do
   end
 
   describe 'validations' do
+    it { should validate_presence_of :full_name }
+    it { should validate_presence_of :code }
+  
     it "is valid when all attributes are present and valid" do
       test_merchant = Merchant.create!(name: "Test Merchant")
       test_coupon = Coupon.create!(
@@ -17,32 +20,6 @@ RSpec.describe Coupon do
       )
       expect(test_coupon).to be_valid
       expect(test_coupon.persisted?).to be true
-    end
-
-    it "is invalid without a name" do
-      test_merchant = Merchant.create!(name: "Test Merchant")
-      test_coupon = Coupon.new(
-        full_name: nil,
-        code: "Ten percent",
-        percent_off: 10,
-        active: false,
-        merchant_id: test_merchant.id
-      )
-      expect(test_coupon).to_not be_valid
-      expect(test_coupon.errors[:full_name]).to include("can't be blank")
-    end
-
-    it "is invalid without a code" do
-      test_merchant = Merchant.create!(name: "Test Merchant")
-      test_coupon = Coupon.new(
-        full_name: "My first test coupon.",
-        code: nil,
-        percent_off: 10,
-        active: false,
-        merchant_id: test_merchant.id
-      )
-      expect(test_coupon).to_not be_valid
-      expect(test_coupon.errors[:code]).to include("can't be blank")
     end
 
     it "is invalid if the code is not unique" do
@@ -64,12 +41,7 @@ RSpec.describe Coupon do
       expect(test_coupon2).to_not be_valid
       expect(test_coupon2.errors[:code]).to include("has already been taken")
     end
-  end
-
-    
-    
-    # it { should validate_inclusion_of(:status).in_array(%w(shipped packaged returned)) }
-  
+  end  
 end
 
  
