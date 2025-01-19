befacfc346ec5ee2a752bf5b4769f15bea50c76c
Kristin Weiland <kristinweiland@gmail.com>
2025-01-19 10:53:44 -0800

    Add an invoice factory, association testing, validation testing, and the start to instance method tests and coupon must be active method in the model.

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,7 +1,11 @@
-FactoryBot.define do
+ FactoryBot.define do
   factory :invoice do
     status { "shipped" }
     customer
     merchant
+
+    trait :with_coupon do
+      association :coupon
+    end
   end
 end
